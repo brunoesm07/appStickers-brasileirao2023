fc8dddcc53abc3c8ad0a5fe73479244f174a6b5d
brmartin | Bruno Martins <69209788+brunoesm07@users.noreply.github.com>
2023-03-31 18:34:07 -0300

    error

--- brasileirao-api/src/main/java/br/com/brmartin/brasileiraoapi/TimesRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.brmartin.brasileiraoapi;
-
-import java.util.List;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TimesRepository extends MongoRepository<Time, String> {
-    List<Time> findByOrderByRanking();
-}
